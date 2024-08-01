b2c6866234366d975e7c3ff88770e4fe40f87af6
issantos <issantos@bionexo.com>
2024-08-01 01:18:58 -0300

    inclusao de novos testes

--- cypress/e2e/calcularFrete.cy.js
+++ cypress/e2e/calcularFrete.cy.js
@@ -1,14 +1,67 @@
 /// <reference types="cypress" />
-describe("Quotation module for buyer users", () => {
 
-    beforeEach(() => {
-        cy.clearCookies();
-        cy.clearLocalStorage();
-      });
+describe("Suite de testes calcular cotações de frete", () => {
 
-    it("Validar calculo de frete com sucesso", () => {
-       cy.visitarUrl();
-       cy.wait(10000)
-       cy.InformeOrigemValida();
-    });
+  it("Calcular frete com sucesso", () => {
+
+    const dados = {
+      cepOrigem: "08090-284",
+      altura: "2",
+      largura: "11",
+      comprimento: "16",
+      cepDestino: "05407-002"
+    }
+    cy.InformeCepOrigem(dados.cepOrigem)
+    cy.InformeDimensoesEmbalagem(dados.altura, dados.largura, dados.comprimento)
+    cy.InformeDestino(dados.cepDestino)
+    cy.CalcularFreteDesconto()
+    cy.wait(3000)
+    cy.ValidoResultadoSucesso()
+  });
+
+  it("Calcular frete sem informar o cep de origem", () => {
+
+    const dados = {
+      altura: "2",
+      largura: "11",
+      comprimento: "16",
+      cepDestino: "05407-002"
+    }
+
+    cy.InformeDimensoesEmbalagem(dados.altura, dados.largura, dados.comprimento)
+    cy.InformeDestino(dados.cepDestino)
+    cy.CalcularFreteDesconto()
+    cy.ValidoErroCepOrigem()
+  });
+
+  it("Calcular frete sem informar o cep de destino", () => {
+
+    const dados = {
+      cepOrigem: "08090-284",
+      altura: "2",
+      largura: "11",
+      comprimento: "16",
+    }
+
+    cy.InformeCepOrigem(dados.cepOrigem)
+    cy.InformeDimensoesEmbalagem(dados.altura, dados.largura, dados.comprimento)
+    cy.CalcularFreteDesconto()
+    cy.ValidoErroCepDestino()
+  });
+
+  it("Calcular frete informando dimensões invalidas para embalagem", () => {
+    const dados = {
+      cepOrigem: "08090-284",
+      altura: "0.3",
+      largura: "7",
+      comprimento: "12",
+      cepDestino: "05407-002"
+    }
+    cy.InformeCepOrigem(dados.cepOrigem)
+    cy.InformeDimensoesEmbalagem(dados.altura, dados.largura, dados.comprimento)
+    cy.InformeDestino(dados.cepDestino)
+    cy.CalcularFreteDesconto()
+    cy.ValidaErroMedidaEmb()
+  });
+
 });
--- cypress/support/calcularFrete.js
+++ cypress/support/calcularFrete.js
@@ -1,10 +1,68 @@
 const elements = {
-    inputCep: "#originPostcode",
+    inputCepOrigem: "#originPostcode",
     selectEmbalagem: "#object_format",
-
+    selectCaixa: ".Mui-selected",
+    selectPeso: "#weight",
+    selectOptionPeso: '[data-value="0.3"]',
+    inputAltura: "#packageHeight",
+    inputLargura: "#packageWidth",
+    inputComprimento: "#packageDepth",
+    inputCepDestino: "#destinationPostcode",
+    buttonCalcularFrete: '[data-cy="calculator-submit"]',
+    labelMensagemOk: "#resultsArea",
+    labelErroCep: "#originPostcode-helper-text",
+    labelErroCepDestino: "#destinationPostcode-helper-text",
+    labelErroAlturaMinima: "#packageHeight-helper-text",
+    labelErroLarguraMinima: "#packageWidth-helper-text",
+    labelErroCompMinimo: "#packageDepth-helper-text"
 };
 
-Cypress.Commands.add('InformeOrigemValida', () => {
-    cy.EscreverNoCampo(elements.inputCep, '06236-110')
+Cypress.Commands.add('InformeCepOrigem', (cepOrigem) => {
+
+    cy.EscreverNoCampo(elements.inputCepOrigem, cepOrigem)
+})
+
+Cypress.Commands.add('InformeDimensoesEmbalagem', (altura, largura, comprimento) => {
+
     cy.clicarNoElemento(elements.selectEmbalagem)
+    cy.clicarNoElemento(elements.selectCaixa)
+    cy.clicarNoElemento(elements.selectPeso)
+    cy.clicarNoElemento(elements.selectOptionPeso)
+    cy.EscreverNoCampo(elements.inputAltura, altura)
+    cy.EscreverNoCampo(elements.inputLargura, largura)
+    cy.EscreverNoCampo(elements.inputComprimento, comprimento)
+})
+
+Cypress.Commands.add('InformeDestino', (destino) => {
+
+    cy.EscreverNoCampo(elements.inputCepDestino, destino)
+})
+
+Cypress.Commands.add('CalcularFreteDesconto', () => {
+
+    cy.clicarNoElemento(elements.buttonCalcularFrete)
+    cy.wait(2000)
+
+})
+Cypress.Commands.add('ValidoResultadoSucesso', () => {
+    
+    cy.VerificarSeCampoContemTexto(elements.labelMensagemOk, 'SELECIONE A MODALIDADE')
+})
+
+Cypress.Commands.add('ValidoErroCepOrigem', () => {
+    
+    cy.ValidarSeCampoContemMensagem(elements.labelErroCep, 'CEP de origem é obrigatório')
+    
+})
+
+Cypress.Commands.add('ValidoErroCepDestino', () => {
+    
+    cy.ValidarSeCampoContemMensagem(elements.labelErroCepDestino, 'CEP de destino é obrigatório')
+})
+
+Cypress.Commands.add('ValidaErroMedidaEmb', () => {
+    
+    cy.ValidarSeCampoContemMensagem(elements.labelErroAlturaMinima, 'Altura mínima 0.4 cm.')
+    cy.ValidarSeCampoContemMensagem(elements.labelErroLarguraMinima, 'Largura mínima 8 cm.')
+    cy.ValidarSeCampoContemMensagem(elements.labelErroCompMinimo, 'Comprimento mínimo 13 cm.')
 })
